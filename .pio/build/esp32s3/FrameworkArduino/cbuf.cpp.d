26478ec4b790d8d911af01853e45b75e249dfd0f
Michael who? <notabot.chat@gmail.com>
2026-04-21 16:47:57 +0000

    Cleaned up temp folders

--- .pio/build/esp32s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32s3/FrameworkArduino/cbuf.cpp.o: \
- /Users/xumic/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /Users/xumic/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32s3/libc40/Adafruit NeoPixel/Adafruit_Neopixel_RP2.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32s3/libc40/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp.o: \
- .pio/libdeps/esp32s3/Adafruit\ NeoPixel/Adafruit_Neopixel_RP2.cpp
